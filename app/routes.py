d001844eddd906e3de6865f3e9290a919aaf3672
Your Name <you@example.com>
2019-09-26 15:19:29 +0100

    jinja, render_template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Fileo'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Japan!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Cars movie was excellent!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
